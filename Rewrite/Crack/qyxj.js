5d1bd5da50c6d418c867f4ec93584e5007b191a4
YHA <48016809+YH-A@users.noreply.github.com>
2022-02-10 15:26:21 +0800

    Update qyxj.js

--- Rewrite/Crack/qyxj.js
+++ Rewrite/Crack/qyxj.js
@@ -2,11 +2,17 @@
 
 #轻颜相机VIP(By @s y)
 
+[rewrite_local]
+
 ^https:\/\/commerce-api\.faceu\.mobi\/commerce\/v1\/subscription\/user_info url script-response-body nzw9314/Script/qyxj.js
+
+[mitm] 
 
 hostname = commerce-api.faceu.mobi
 
-
+*
+*
+*/
 
 let obj = JSON.parse($response.body);
 obj ={
