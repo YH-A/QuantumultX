b48371ae9a5f789a42a5d24b25e32081eca733ec
YHA <48016809+YH-A@users.noreply.github.com>
2022-02-12 19:46:35 +0800

    Update qyxj.js

--- Rewrite/Crack/qyxj.js
+++ Rewrite/Crack/qyxj.js
@@ -11,6 +11,21 @@ hostname=commerce-api.faceu.mobi
 
 */
 let obj = JSON.parse($response.body);
-obj.data["end_time"] = 3725012184;
-obj.data["flag"] = true;
+obj ={
+"ret":"0",
+"errmsg":"Success",
+"systime":"",
+"data":{
+"flag":true,
+"start_time":1579094492,
+"end_time":3725012184,
+"is_first_subscribe":false,
+"is_cancel_subscribe":false,
+"uid":"4089778978429341",
+"subscribe_uid":"70851259049"
+}
+};
 $done({body: JSON.stringify(obj)});
+
+//轻颜相机
+
