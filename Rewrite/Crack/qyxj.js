e38047342ac1a67b5ef3b096541363f75d13b7cb
YHA <48016809+YH-A@users.noreply.github.com>
2022-02-12 19:23:41 +0800

    Update qyxj.js

--- Rewrite/Crack/qyxj.js
+++ Rewrite/Crack/qyxj.js
@@ -5,3 +5,12 @@
 [rewrite_local]
 
 ^https:\/\/commerce-api\.faceu\.mobi\/commerce\/v1\/subscription\/user_info url script-response-body https://raw.githubusercontent.com/YH-A/QuantumultX/main/Rewrite/Crack/qyxj.js
+
+
+hostname=commerce-api.faceu.mobi
+
+*/
+let obj = JSON.parse($response.body);
+obj.data["end_time"] = 3725012184;
+obj.data["flag"] = true;
+$done({body: JSON.stringify(obj)});
