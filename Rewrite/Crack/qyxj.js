09550626cf201664d40131785fe90f8239b3b2b2
YHA <48016809+YH-A@users.noreply.github.com>
2022-02-12 19:15:59 +0800

    Update qyxj.js

--- Rewrite/Crack/qyxj.js
+++ Rewrite/Crack/qyxj.js
@@ -4,31 +4,4 @@
 
 [rewrite_local]
 
-^https:\/\/commerce-api\.faceu\.mobi\/commerce\/v1\/subscription\/user_info url script-response-body nzw9314/Script/qyxj.js
-
-[mitm] 
-
-hostname = commerce-api.faceu.mobi
-
-*
-*
-*/
-
-let obj = JSON.parse($response.body);
-obj ={
-"ret":"0",
-"errmsg":"Success",
-"systime":"",
-"data":{
-"flag":true,
-"start_time":1579094492,
-"end_time":3725012184,
-"is_first_subscribe":false,
-"is_cancel_subscribe":false,
-"uid":"4089778978429341",
-"subscribe_uid":"70851259049"
-}
-};
-$done({body: JSON.stringify(obj)});
-
-//轻颜相机
+^https:\/\/commerce-api\.faceu\.mobi\/commerce\/v1\/subscription\/user_info url script-response-body https://raw.githubusercontent.com/YH-A/QuantumultX/main/Rewrite/Crack/qyxj.js
