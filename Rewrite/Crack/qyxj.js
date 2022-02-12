b40006cf87a2a2cf6335fab54f4b8472d2301991
YHA <48016809+YH-A@users.noreply.github.com>
2022-02-12 19:55:35 +0800

    Update qyxj.js

--- Rewrite/Crack/qyxj.js
+++ Rewrite/Crack/qyxj.js
@@ -4,28 +4,27 @@
 
 [rewrite_local]
 
-^https:\/\/commerce-api\.faceu\.mobi\/commerce\/v1\/subscription\/user_info url script-response-body https://raw.githubusercontent.com/YH-A/QuantumultX/main/Rewrite/Crack/qyxj.js
+https://(commerce-.*api|pay).(faceu|wecut).(com|mobi)/(commerce|apple)/(iosAppVerifyReceipt.php|v1/subscription/user_info) url script-response-body https://raw.githubusercontent.com/YH-A/QuantumultX/main/Rewrite/Crack/qyxj.js
 
 
 hostname=commerce-api.faceu.mobi
 
 */
+const path1 = "/commerce/v1/subscription/user_info";
+const path2 = "/apple/iosAppVerifyReceipt.php";
+
 let obj = JSON.parse($response.body);
-obj ={
-"ret":"0",
-"errmsg":"Success",
-"systime":"",
-"data":{
-"flag":true,
-"start_time":1579094492,
-"end_time":3725012184,
-"is_first_subscribe":false,
-"is_cancel_subscribe":false,
-"uid":"4089778978429341",
-"subscribe_uid":"70851259049"
+
+if ($request.url.indexOf(path1) != -1){
+obj.data.start_time = 1584674770;
+obj.data.end_time = 4077660370;
+obj.data.is_cancel_subscribe = true;
+obj.data.flag = true;
 }
-};
+if ($request.url.indexOf(path2) != -1){
+ obj.data = {
+    "isValid": 1,
+    "expiresTs": 4077660370
+}
+}
 $done({body: JSON.stringify(obj)});
-
-//轻颜相机
-
