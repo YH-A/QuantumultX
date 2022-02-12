7fd17a29ea02bc49f6d0e1ec5885396634eebbe0
YHA <48016809+YH-A@users.noreply.github.com>
2022-02-12 21:37:13 +0800

    Create qyxj.js

---
+++ Rewrite/Crack/qyxj.js
@@ -0,0 +1,18 @@
+const path1 = "/commerce/v1/subscription/user_info";
+const path2 = "/apple/iosAppVerifyReceipt.php";
+
+let obj = JSON.parse($response.body);
+
+if ($request.url.indexOf(path1) != -1){
+obj.data.start_time = 1584674770;
+obj.data.end_time = 4077660370;
+obj.data.is_cancel_subscribe = true;
+obj.data.flag = true;
+}
+if ($request.url.indexOf(path2) != -1){
+ obj.data = {
+    "isValid": 1,
+    "expiresTs": 4077660370
+}
+}
+$done({body: JSON.stringify(obj)});
