22242753ed5c31a24e484eadcc9cbfbfad3b1b12
YHA <48016809+YH-A@users.noreply.github.com>
2022-02-12 19:10:26 +0800

    Update RNI films.js

--- Rewrite/Crack/RNI films.js
+++ Rewrite/Crack/RNI films.js
@@ -13,17 +13,4 @@
 [rewrite_local]
 
 # ～ RNI解锁高级会员（20220212）@ddgksf2013
-https://pro-status-service-prod.azurewebsites.net/api/item url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/rf.js
-
-[mitm] 
-
-hostname=pro-status-service-prod.azurewebsites.net
-
-*/
-
-
-
-var ddgksf2013 = JSON.parse($response.body);
-ddgksf2013.FilmsProStatus = 1
-ddgksf2013.AeroProStatus = 1
-$done({body: JSON.stringify(ddgksf2013)});
+https://pro-status-service-prod.azurewebsites.net/api/item url script-response-body https://raw.githubusercontent.com/YH-A/QuantumultX/main/Rewrite/Crack/RNI%20films.js
