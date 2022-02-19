2c0d5df28a2f133d5d1faee03bf636f07a1595aa
YHA <48016809+YH-A@users.noreply.github.com>
2022-02-19 23:59:44 +0800

    Update fb.js

--- Rewrite/Crack/fb.js
+++ Rewrite/Crack/fb.js
@@ -12,7 +12,7 @@
  
 [rewrite_local]
 
-# ～ filebox解鎖會員權限（2022-02-18）@ddgksf2013
+# ～ 解鎖會員權限（2022-02-18）@ddgksf2013
 ^https?:\/\/api\.revenuecat\.com\/v\d\/subscribers\/(\$RCAnonymousID\%)?(\w)+$ url script-echo-response https://raw.githubusercontent.com/YH-A/QuantumultX/main/FB.js
 ^https?:\/\/api\.revenuecat\.com\/v\d\/receipts$ url script-echo-response https://raw.githubusercontent.com/YH-A/QuantumultX/main/FB.js
 
