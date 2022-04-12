dde68d3d27b0cee93fcd2f6c8f7d5214e22618e1
YHA <48016809+YH-A@users.noreply.github.com>
2022-04-12 18:38:42 +0800

    Create beeg.js

---
+++ Rewrite/Crack/beeg.js
@@ -0,0 +1,21 @@
+/*
+app下载地址：商店搜索：贝尔儿歌
+#贝尔儿歌
+重写
+^http://iface.beva.com/order/device/vip url script-response-body bweg.js
+主机
+iface.beva.com
+*/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/order/device/vip';
+
+if (url.indexOf(vip) != -1) {
+    obj.is_vip = 1;
+    obj.end_time = 7961094083;
+    body = JSON.stringify(obj);
+}
+$done({body});
