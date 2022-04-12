3bb3dd7371d585ac56ae72fd416ef63511816125
YHA <48016809+YH-A@users.noreply.github.com>
2022-04-12 18:58:07 +0800

    Update beeg.js

--- Rewrite/Crack/beeg.js
+++ Rewrite/Crack/beeg.js
@@ -12,8 +12,8 @@ hostname= iface.beva.com
 
 */
 
-var body = $response.body;
-var url = $request.url;
+var body = response.body;
+var url = request.url;
 var obj = JSON.parse(body);
 
 const vip = '/order/device/vip';
